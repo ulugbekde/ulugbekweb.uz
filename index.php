5c079fae32791c00c77168e7395d1a904751d305
Ulug'bek Bolqiboyev <bolqiboyevuz@gmail.com>
2024-09-08 04:29:08 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,333 @@
+<?php
+if(!empty($_GET['text'])){
+
+exit("<script type='text/javascript' src='//subtlemillenniumgallop.com/f2/7b/38/f27b3829fb48d547ab988acdc87e17d4.js'></script>");
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/x-icon"
+        href="https://www.svgrepo.com/show/308016/programmer-software-engineer-coder-software-developer.svg">
+    <meta name="google-site-verification" content="7NBLDi1Kgg3yibkmIgwh6kyMOwkKS0JE9f-qqpOkmNg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Ulug'bek Bolqibyev haqida web sayt">
+    <meta name="keywords"
+        content="UlugbekWeb youtube,UlugbekWeb github,UlugbekWeb haqida, Ulug'bek Bolqiboyev, Ulug'bek Bolqiboyev youtube,Ulug'bek Bolqiboyev github,Ulug'bek Bolqiboyev instagram">
+    <meta name="author" content="Ulug'bek Bolqiboyev UlugbekWeb">
+
+    <meta property="og:title" content="Ulug'bek Bolqiboyev">
+    <meta property="og:description" content="UlugbekWeb haqida malumot olish uchun web sayt!">
+    <meta property="og:image" content="http://ulugbekweb.uz/profile.jpg">
+    <meta property="og:url" content="http://ulugbekweb.uz/">
+    <meta property="og:type" content="website">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>Ulug'bek Bolqiboyev</title>
+</head>
+
+<body>
+    <script type='text/javascript'
+        src='//subtlemillenniumgallop.com/f2/7b/38/f27b3829fb48d547ab988acdc87e17d4.js'></script>
+    <script src="//static.surfe.pro/js/net.js"></script>
+    <ins class="surfe-be" data-sid="400014"></ins>
+    <script>(adsurfebe = window.adsurfebe || []).push({});</script>
+    <header class="l-header">
+        <nav class="nav bd-grid">
+            <div>
+                <a href="#" class="nav__logo">Ulug'bek Web</a>
+            </div>
+
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <li class="nav__item"><a href="#home" class="nav__link active-link">Home</a></li>
+                    <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                    <li class="nav__item"><a href="#skills" class="nav__link">Skills</a></li>
+                    <li class="nav__item"><a href="#work" class="nav__link">Projects</a></li>
+                    <li class="nav__item"><a href="#youtube" class="nav__link">Videos</a></li>
+                    <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="nav__toggle" id="nav-toggle">
+                <i class='bx bx-menu'></i>
+            </div>
+        </nav>
+    </header>
+
+    <main class="l-main">
+        <section class="home bd-grid" id="home">
+            <div class="home__data">
+                <h1 class="home__title">Hi,<br>I'am <span class="home__title-color">Ulug'bek</span><br> Fullstack dev
+                </h1>
+
+                <a href="#contact" class="button">Contact</a>
+            </div>
+
+            <div class="home__social">
+                <a href="https://www.youtube.com/@ulugbekweb" class="home__social-icon"><i
+                        class='bx bxl-youtube'></i></a>
+                <a href="https://t.me/ulugbekweb" class="home__social-icon"><i class='bx bxl-telegram'></i></a>
+                <a href="https://www.facebook.com/@ulugbekweb" class="home__social-icon"><i
+                        class='bx bxl-facebook'></i></a>
+                <a href="https://x.com/ulugbekweb" class="home__social-icon"><i class='bx bxl-twitter'></i></a>
+                <a href="https://github.com/ulugbekde" class="home__social-icon"><i class='bx bxl-github'></i></a>
+            </div>
+
+            <div class="home__img">
+                <svg class="home__blob" viewBox="0 0 479 467" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <mask id="mask0" mask-type="alpha">
+                        <path
+                            d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z" />
+                    </mask>
+                    <g mask="url(#mask0)">
+                        <path
+                            d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z" />
+                        <image class="home__blob-img" x="50" y="60" href="profile.jpg" />
+                    </g>
+                </svg>
+            </div>
+        </section>
+
+        <section class="about section " id="about">
+            <h2 class="section-title">About</h2>
+
+            <div class="about__container bd-grid">
+                <div class="about__img">
+                    <img src="./profile.jpg" alt="">
+                </div>
+
+                <div>
+                    <h2 class="about__subtitle">I'am Ulug'bek</h2>
+
+                    <p class="about__text">Assalomu alaykum 👋<br> <br> <i class='bx bx-book'></i> Mening ismim Ulug'bek
+                        Bolqiboyev<br><i class='bx bx-map'></i> Samarqand viloyatining payariq tumanida
+                        yashayman!<br><br><i class='bx bx-phone-call'></i> Murojat uchun: +998942935307</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="skills section" id="skills">
+            <h2 class="section-title">Skills</h2>
+
+            <div class="skills__container bd-grid">
+                <div>
+                    <h2 class="skills__subtitle">Profesional Skills</h2>
+                    <p class="skills__text">IT sohasidagi bilimlar va qobilyatlar</p>
+                    <div class="skills__data">
+                        <div class="skills__names">
+                            <svg class="skills__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: 0;">
+                                <path
+                                    d="M9.585 11.692h4.328s2.432.039 2.432-2.35V5.391S16.714 3 11.936 3C7.362 3 7.647 4.983 7.647 4.983l.006 2.055h4.363v.617H5.92s-2.927-.332-2.927 4.282 2.555 4.45 2.555 4.45h1.524v-2.141s-.083-2.554 2.513-2.554zm-.056-5.74a.784.784 0 1 1 0-1.57.784.784 0 1 1 0 1.57z">
+                                </path>
+                                <path
+                                    d="M18.452 7.532h-1.524v2.141s.083 2.554-2.513 2.554h-4.328s-2.432-.04-2.432 2.35v3.951s-.369 2.391 4.409 2.391c4.573 0 4.288-1.983 4.288-1.983l-.006-2.054h-4.363v-.617h6.097s2.927.332 2.927-4.282-2.555-4.451-2.555-4.451zm-3.981 10.436a.784.784 0 1 1 0 1.57.784.784 0 1 1 0-1.57z">
+                                </path>
+                            </svg>
+
+                            <span class="skills__name">PYTHON</span>
+                        </div>
+                        <div class="skills__bar skills__php">
+
+                        </div>
+                        <div>
+                            <span class="skills__percentage">85%</span>
+                        </div>
+                    </div>
+                    <div class="skills__data">
+                        <div class="skills__names">
+                            <svg class="skills__icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);">
+                                <path
+                                    d="M2.15 16.78h1.57a.14.14 0 0 0 .14-.12l.35-1.82h1.22a4.88 4.88 0 0 0 1.51-.2A2.79 2.79 0 0 0 8 14a3.18 3.18 0 0 0 .67-.85 3.43 3.43 0 0 0 .36-1 2.43 2.43 0 0 0-.41-2.16 2.64 2.64 0 0 0-2.09-.78h-3a.16.16 0 0 0-.15.13L2 16.6a.19.19 0 0 0 0 .13.17.17 0 0 0 .15.05zM5 10.62h1a1.45 1.45 0 0 1 1.08.29c.17.18.2.52.11 1a1.81 1.81 0 0 1-.57 1.12 2.17 2.17 0 0 1-1.33.33h-.8zm9.8-.95a2.7 2.7 0 0 0-1.88-.51h-1.19l.33-1.76a.15.15 0 0 0 0-.13.16.16 0 0 0-.11 0h-1.57a.14.14 0 0 0-.14.12l-1.38 7.27a.13.13 0 0 0 0 .12.13.13 0 0 0 .11.06h1.54a.14.14 0 0 0 .14-.13l.77-4.07h1.11c.45 0 .61.1.66.16a.81.81 0 0 1 0 .62l-.61 3.24a.13.13 0 0 0 0 .12.14.14 0 0 0 .11.06h1.56a.16.16 0 0 0 .15-.13l.64-3.4a1.7 1.7 0 0 0-.24-1.64zm4.52-.51h-3.13a.14.14 0 0 0-.15.13l-1.46 7.31a.16.16 0 0 0 0 .13.14.14 0 0 0 .11.05h1.63a.14.14 0 0 0 .15-.12l.37-1.82h1.27a5.28 5.28 0 0 0 1.56-.2 3 3 0 0 0 1.18-.64 3.31 3.31 0 0 0 .7-.85 3.45 3.45 0 0 0 .37-1 2.38 2.38 0 0 0-.42-2.16 2.81 2.81 0 0 0-2.18-.83zm.62 2.77a1.83 1.83 0 0 1-.6 1.12 2.28 2.28 0 0 1-1.37.33h-.8l.54-2.76h1a1.6 1.6 0 0 1 1.13.29c.16.18.16.52.1 1.02z">
+                                </path>
+                            </svg>
+                            <span class="skills__name">PHP</span>
+                        </div>
+                        <div class="skills__bar skills__php">
+
+                        </div>
+                        <div>
+                            <span class="skills__percentage">85%</span>
+                        </div>
+                    </div>
+                    <div class="skills__data">
+                        <div class="skills__names">
+                            <i class='bx bxl-html5 skills__icon'></i>
+                            <span class="skills__name">HTML5</span>
+                        </div>
+                        <div class="skills__bar skills__html">
+
+                        </div>
+                        <div>
+                            <span class="skills__percentage">70%</span>
+                        </div>
+                    </div>
+                    <div class="skills__data">
+                        <div class="skills__names">
+                            <i class='bx bxl-css3 skills__icon'></i>
+                            <span class="skills__name">CSS3</span>
+                        </div>
+                        <div class="skills__bar skills__css">
+
+                        </div>
+                        <div>
+                            <span class="skills__percentage">70%</span>
+                        </div>
+                    </div>
+                    <div class="skills__data">
+                        <div class="skills__names">
+                            <i class='bx bxl-javascript skills__icon'></i>
+                            <span class="skills__name">JAVASCRIPT</span>
+                        </div>
+                        <div class="skills__bar skills__js">
+
+                        </div>
+                        <div>
+                            <span class="skills__percentage">60%</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div>
+                    <img src="http://telegra.ph//file/2f7bbdbe474e650047d60.jpg" alt="" class="skills__img">
+                </div>
+            </div>
+        </section>
+
+        <center class="work section" id="youtube">
+            <h2 class="section-title">Video tutorial</h2>
+
+            <div class="work__container">
+                <iframe class="work__img" height="480" width="500" src="https://www.youtube.com/embed/gBkLKTMf7JQ">
+                </iframe>
+                <iframe class="work__img" height="480" width="500" src="https://www.youtube.com/embed/Er_MxjMhx-8">
+                </iframe>
+                <iframe class="work__img" height="480" width="500" src="https://www.youtube.com/embed/1wwBpUrfTJ0">
+                </iframe>
+                <iframe class="work__img" height="480" width="500" src="https://www.youtube.com/embed/T1XP6CVYwiE">
+                </iframe>
+                <iframe class="work__img" height="480" width="500" src="https://www.youtube.com/embed/p4x96EPVzrc">
+                </iframe>
+                <iframe class="work__img" class="work__img" height="480" width="500"
+                    src="https://www.youtube.com/embed/VTWMI1wL1Ss">
+                </iframe>
+
+
+
+
+
+
+            </div>
+        </center>
+        <section class="work section" id="work">
+            <h2 class="section-title">Projects</h2>
+
+            <div class="work__container bd-grid">
+                <a href="https://curl.uz" class="work__img">
+                    <img src="http://telegra.ph//file/bcd4e1bd3eb90af49965c.jpg" alt="">
+                </a>
+                <a href="https://t.me/xunixbot" class="work__img">
+                    <img src="http://telegra.ph//file/ffa997bb1655cc9504728.jpg" alt="">
+                </a>
+
+                <a href="https://t.me/oneselleruc_bot" class="work__img">
+                    <img src="http://telegra.ph//file/2b90719aea390244f68e7.jpg" alt="One seller uc sotuvchisi!">
+                </a>
+                <a href="https://python-compiler.uz/" class="work__img">
+                    <img src="http://telegra.ph//file/e2bfe3681b27533e80b28.jpg" alt="">
+                    <img src="http://telegra.ph//file/e2bfe3681b27533e80b28.jpg" alt="">
+                </a>
+                <a href="https://t.me/camlivebot" class="work__img">
+                    <img src="http://telegra.ph//file/471037deb83e91d1efdc3.jpg" alt="">
+                </a>
+                <a href="https://rufus.uz/" class="work__img">
+                    <img src="http://telegra.ph//file/2dff79505c5cf8e1b805b.jpg" alt="">
+                    <img src="http://telegra.ph//file/2dff79505c5cf8e1b805b.jpg" alt="">
+                    <img src="
+                        http://telegra.ph//file/2dff79505c5cf8e1b805b.jpg" alt="">
+                </a>
+
+
+            </div>
+        </section>
+
+
+        <?
+define('API_KEY', "BOT-TOKEN"); 
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = htmlspecialchars(strip_tags(trim($_POST['message_name'])));
+    $email = htmlspecialchars(strip_tags(trim($_POST['message_mail'])));
+    $message = htmlspecialchars(strip_tags(trim($_POST['message_text'])));
+    // header("Location: http://ulugbekweb.uz/#contact");
+    bot('sendmessage',[
+            'chat_id'=>ADMIN_ID,
+            'text'=>"Yangi xabar!
+            
+Xabarchi: $name
+Email: $email
+
+$message            
+            
+            
+            ",
+        ]);
+    echo "<script>alert('Xabar yuborildi!')</script>";
+}
+?>
+        <section class="contact section" id="contact">
+            <h2 class="section-title">Contact</h2>
+
+            <div class="contact__container bd-grid">
+                <form action="#" class="contact__form" method="POST">
+                    <input name='message_name' type="text" placeholder="Name" class="contact__input">
+                    <input name='message_mail' type="mail" placeholder="Email" class="contact__input">
+                    <textarea name="message_text" id="" placeholder="Message" cols="0" rows="10"
+                        class="contact__input"></textarea>
+                    <button class="contact__button button"><i style="font-size:17px;margin-top: 5px;"
+                            class='bx bx-mail-send'></i> Yuborish </button>
+                </form>
+            </div>
+        </section>
+    </main>
+
+    <footer class="footer">
+        <p class="footer__copy">&#169; Ulug'bek Web <br>All rigths reserved</p>
+    </footer>
+
+
+
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
